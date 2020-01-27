47ce853ae99de42c61bbfa270c51b4ed3a5cd439
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-27 17:58:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,37 +1,18 @@
-# number adding
-from adapt.intent import IntentBuilder
-from mycroft.skills.core import MycroftSkill, intent_handler
-from mycroft.util.log import LOG, getLogger
+def  enter_source_destination(stops):
+    while True:
+        source = input('Enter your boarding bus stop: ')
+        if source in stops:
+            while True:
+                destination = input('Enter your destination: ')
+                if destination in stops:
+                    return source, destination
+                else:
+                    print('Could you please enter a valid destination stop')
+                    continue
+        else:
+            print('Could you please enter a valid boarding point')
+            continue
 
-__author__ = 'pythona620/prasad'
-LOGGER = getLogger(__name__)
-
-class NumberAddingSkill(MycroftSkill):
-
-	first_number = 0  
-	second_number = 0
-	
-	def get_numerical_response(self, dialog):
-		while True:
-			val = self.get_response(dialog)
-			try:
-				val = int(val) #check the value int or not
-				return val
-			except ValueError:
-				self.speak_dialog("invalid.input")
-			except:
-				self.speak_dialog("input.error")
-
-	@intent_handler(IntentBuilder("").require("Adding").optionally("Play").optionally("Suggest"))
-	def handle_start_game_intent(self, message):
-		self.speak_dialog("start.game")
-
-		# get first_numbe
-		first_number = self.get_numerical_response("get.first")
-		# get second_number
-		second_number = self.get_numerical_response("get.second")
-		self.speak("first_number " + " second_number") #print specific keword
-	def stop(self):		
-		pass
-def create_skill():
-	return NumberAddingSkill()
+stops = ['vizag', 'hyderabad', 'vijayawada']
+source, destination = enter_source_destination(stops)
+print('The sourceing point is ', source, 'and the destination is ', destination)
