e1d7cec63e54ad2f53ba776fafa1601b81bf52cf
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-28 19:30:30 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,42 +1,18 @@
 from mycroft.skills.context import adds_context, removes_context
 
-class TeaSkill(MycroftSkill):
-    @intent_handler(IntentBuilder('TeaIntent').require("TeaKeyword"))
-    @adds_context('MilkContext')
-    def handle_tea_intent(self, message):
-        self.milk = False
-        self.speak('Of course, would you like Milk with that?',
-                   expect_response=True)
+    @intent_handler(IntentBuilder().require('PythonPerson').require('Length'))
+    def handle_length(self, message):
+        python = message.data.get('PythonPerson')
+        self.speak('{} is {} cm tall'.format(python, length_dict[python]))
 
-    @intent_handler(IntentBuilder('NoMilkIntent').require("NoKeyword").
-                                  require('MilkContext').build())
-    @removes_context('MilkContext')
-    @adds_context('HoneyContext')
-    def handle_no_milk_intent(self, message):
-        self.speak('all right, any Honey?', expect_response=True)
+    @intent_handler(IntentBuilder().require('PythonPerson').require('WhereFrom'))
+    def handle_from(self, message):
+        python = message.data.get('PythonPerson')
+        self.speak('{} is from {}'.format(python, from_dict[python]))
+   
+# def stop(self):
+# 		pass
 
-    @intent_handler(IntentBuilder('YesMilkIntent').require("YesKeyword").
-                                  require('MilkContext').build())
-    @removes_context('MilkContext')
-    @adds_context('HoneyContext')
-    def handle_yes_milk_intent(self, message):
-        self.milk = True
-        self.speak('What about Honey?', expect_response=True)
-
-    @intent_handler(IntentBuilder('NoHoneyIntent').require("NoKeyword").
-                                  require('HoneyContext').build())
-    @removes_context('HoneyContext')
-    def handle_no_honey_intent(self, message):
-        if self.milk:
-            self.speak('Heres your Tea with a dash of Milk')
-        else:
-            self.speak('Heres your Tea, straight up')
-
-    @intent_handler(IntentBuilder('YesHoneyIntent').require("YesKeyword").
-                                require('HoneyContext').build())
-    @removes_context('HoneyContext')
-    def handle_yes_honey_intent(self, message):
-        if self.milk:
-            self.speak('Heres your Tea with Milk and Honey')
-        else:
-            self.speak('Heres your Tea with Honey')
+# def create_skill():
+# 	return NumberGuessSkill()
+   
