7353a8a298c7b8b34f0a1220b2e5ecb3ece7d982
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-28 19:52:10 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,9 +2,9 @@ from adapt.intent import IntentBuilder
 from mycroft import intent_handler
 
 class PotatoSkill(MycroftSkill):
-	
-	 @intent_handler(IntentBuilder().require(PythonPerson).require(WhereFrom))
-    def handle_from(self, message):
+	@intent_handler(IntentBuilder().require(PythonPerson).require(WhereFrom))
+		
+    	def handle_from(self, message):
         # PythonPerson can be any of the Monty Python members
         python = message.data.get('PythonPerson')
         self.speak('He is from {}'.format(from_dict[python]))
