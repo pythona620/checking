f23cf582689d13f34475c867d1cd76aed058b062
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-28 11:26:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,36 @@
-def  enter_source_destination(stops):
-    while True:
-        source = input('Enter your boarding bus stop: ')
-        if source in stops:
-            while True:
-                destination = input('Enter your destination: ')
-                if destination in stops:
-                    return source, destination
-                else:
-                    print('Could you please enter a valid destination stop')
-                    continue
-        else:
-            print('Could you please enter a valid boarding point')
-            continue
-
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler, intent_file_handler
 stops = ['vizag', 'hyderabad', 'vijayawada']
-source, destination = enter_source_destination(stops)
-print('The sourceing point is ', source, 'and the destination is ', destination)
+class ticket(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+    
+    @intent_handler(IntentBuilder("travel").require("travel").build())
+    
+    def handle_travel_intent(self, source,destination):
+        
+#     def getstop(self,source,destination):
+        source = []
+	    destination = []
+	    while true:
+            source = (source.data.get('source')
+	  	    if source in stops:
+	  		    while True:
+                    destination = (source.data.get('destination')               
+                # destination = input('Enter your destination: ')
+                    if destination in stops:
+                        return source, destination
+                    else:
+                        self.speak_dialog('vaild.destination')
+                        continue
+            else:
+                self.speak_dialog('vaild.boarding')
+                continue
+     def stop(self):
+             source,destination = getstops(stops)
+             self.speak("your friend is going " + source  + " " + "to" + " "+  destination ) 
+                                   
+        pass
+
+def create_skill():
+    return ticket()
