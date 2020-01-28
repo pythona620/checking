5d1170f354123ab2ea39fc2103d7047c4b5997a1
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-28 18:55:38 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,45 +17,45 @@ class NumberGuessSkill(MycroftSkill):
 # 				self.speak_dialog("invalid.input")
 # 			except:
 # 				self.speak_dialog("input.error")	
-# 	lowerBound = 0
-# 	upperBound = 100
-# 	answer = 0
-# 	userGuess = 0
-# 	def get_numerical_response(self, dialog):
-# 	def get_numerical_response(self, dialog):
-# 		while True:
-# 			val = self.get_response(dialog)
-# 			try:
-# 				val = int(val)
-# 				return val
-# 			except ValueError:
-# 				self.speak_dialog("invalid.input")
-# 			except:
-# 				self.speak_dialog("input.error")
+	lowerBound = 0
+	upperBound = 100
+	answer = 0
+	userGuess = 0
+	def get_numerical_response(self, dialog):
+	def get_numerical_response(self, dialog):
+		while True:
+			val = self.get_response(dialog)
+			try:
+				val = int(val)
+				return val
+			except ValueError:
+				self.speak_dialog("invalid.input")
+			except:
+				self.speak_dialog("input.error")
 	@intent_handler(IntentBuilder("").require("NumberGuess").optionally("Play").optionally("Suggest"))
 	def handle_start_game_intent(self, message):
 		self.speak_dialog("start.game")
 		# get lower bound
-		lowerBound = message.data.get("sor")
+# 		lowerBound = message.data.get("sor")
 		# get upper bound
-		upperBound = message.data.get("get.des")
+# 		upperBound = message.data.get("get.des")
 		
-		if lowerBound is not None:
-			self.speak(lowerBound + " "+upperBound)
-# 		answer = randint(lowerBound, upperBound)
-# 		userGuess = lowerBound - 1
-# 		while userGuess != answer:
-# 			userGuess = self.get_numerical_response("guess")
-# 			if userGuess < answer:
-# 				self.speak_dialog("too.low")
-# 			elif userGuess > answer:
-# 				self.speak_dialog("too.high")
-# 		self.speak_dialog("correct")
+# 		if lowerBound is not None:
+# 			self.speak(lowerBound + " "+upperBound)
+		answer = randint(lowerBound, upperBound)
+		userGuess = lowerBound - 1
+		while userGuess != answer:
+			userGuess = self.get_numerical_response("guess")
+			if userGuess < answer:
+				self.speak_dialog("too.low")
+			elif userGuess > answer:
+				self.speak_dialog("too.high")
+		self.speak_dialog("correct")
 	def stop(self):
 		pass
-# 		lowerBound, upperBound = 0, 100
-# 		answer = 0
-# 		userGuess = answer
-# 		return True
+		lowerBound, upperBound = 0, 100
+		answer = 0
+		userGuess = answer
+		return True
 def create_skill():
 	return NumberGuessSkill()
