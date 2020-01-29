a2d0bdf35d388ae755f1c35e7b0eba6e49df9d4e
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-29 18:01:13 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,38 +1,50 @@
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG, getLogger
-
 from random import randint
-
 __author__ = 'python'
 LOGGER = getLogger(__name__)
-
-
-stops = {"vizag","hyd","sec","vijayawada"}
-class NumberGuessSkill(MycroftSkill):
-
-# 	def get_numerical_response(self, dialog):
-# 		while True:
-# 			val = self.get_response(dialog)
-# 	def get_stops(self, stops):
-# 		stoplist = []
-# 		if stops !=lower:
-# 			self.speak(tested ok)
-	def get_numerical_response(self, dialog):	
+class checkingSkill(MycroftSkill):
+	lowerBound = {"vizag","sec","hyd","abc"}
+	upperBound = {"vizag","sec","hyd","abc"}
+	answer = 0
+	userGuess = 0
+	def get_numerical_response(self, dialog):
+		while True:
+			val = self.get_response(dialog)
+			try:
+				val = int(val)
+				return val
+			except ValueError:
+				self.speak_dialog("invalid.input")
+			except:
+				self.speak_dialog("input.error")
 	@intent_handler(IntentBuilder("").require("NumberGuess").optionally("Play").optionally("Suggest"))
-# 	def handle_start_game_intent(self, message):
-# 		self.speak_dialog("start.game")
-# 	def get_numerical_response(self, dialog):
-# 		# get lower bound
+	def handle_start_game_intent(self, message):
+		self.speak_dialog("start.game")
+		# get lower bound
 		lowerBound = self.get_numerical_response("get.lower")
-# 		# get upper bound
+		# get upper bound
 		upperBound = self.get_numerical_response("get.upper")
-# 		if lowerBound is not None:
-# 			self.speak(lowerBound + " "+upperBound)
-#         		else:
-#             		     self.speak('you are not given any specific word!')
+# 		answer = randint(lowerBound, upperBound)
+# 		userGuess = lowerBound - 1
+		while True:
+        		source = lowerBound
+        		if source in stops:
+            			while True:
+                			destination = upperBound
+                			if destination in stops:
+                    				return lowerBound, upperBound
+                			else:
+                    				self.speak("Could you please enter a valid destination stop")
+                    				continue
+        		else:
+            			self.speak("Could you please enter a valid boarding point")
+            		continue
 	def stop(self):
-		pass
-
+# 		lowerBound, upperBound = 0, 100
+# 		answer = 0
+# 		userGuess = answer
+		return True
 def create_skill():
-	return NumberGuessSkill()
+	return checkingSkill()
