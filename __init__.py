554ceb92781754f241e985a1731b0fb2dc5c0f66
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-29 18:50:15 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,27 +13,27 @@ class checkingSkill(MycroftSkill):
 	def handle_start_game_intent(self, message):
 		self.speak_dialog("start.game")
 		# get lower bound
-		myname = self.get_names("get.lower")
+		lowerBound = self.get_names("get.lower")
 		# get myfriendname
-		myfriendname = self.get_names("get.upper")
-# 	def  enter_source_destination(stops):
-# 		while True:
-#         		source = lowerBound
-#         		if source in stops:
-#             			while True:
-#                 			destination = upperBound
-#                 			if destination in stops:
-#                     				return source, destination
-#                 			else:
-#                     				self.speak('Could you please enter a valid destination stop')
-#                     				continue
-#        			 else:
-#             			self.speak('Could you please enter a valid boarding point')
-#            			continue
+		upperBound = self.get_names("get.upper")
+	def  enter_source_destination(stops):
+		while True:
+        		source = lowerBound
+        		if source in stops:
+            			while True:
+                			destination = upperBound
+                			if destination in stops:
+                    				return source, destination
+                			else:
+                    				self.speak('Could you please enter a valid destination stop')
+                    				continue
+       			 else:
+            			self.speak('Could you please enter a valid boarding point')
+           			continue
 
-# 	stops = {"vizag", "hyderabad", "vijayawada"}
-# 	source, destination = enter_source_destination(stops)
-# 	self.speak('The sourceing point is '+ source+ 'and the destination is '+ destination)
+	stops = {"vizag", "hyderabad", "vijayawada"}
+	source, destination = enter_source_destination(stops)
+	self.speak('The sourceing point is '+ source+ 'and the destination is '+ destination)
 
 	def stop(self):
 		pass
