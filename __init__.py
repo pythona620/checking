acfcd8d5c6b5233b7c68b2557e77053f0ab8bd9b
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-29 19:18:31 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,30 +6,30 @@ __author__ = 'python'
 LOGGER = getLogger(__name__)
 class checkingSkill(MycroftSkill):
 	stops = {"vizag","sec","hyd","abc"}
-	def get_names(self, dialog):  #get input from mic
+	def get_stops(self, dialog):  #get input from mic
 		yip = self.get_response(dialog) 
 		return yip
 	@intent_handler(IntentBuilder("").require("travel").optionally("Play").optionally("Suggest"))
 	def handle_start_game_intent(self, message):
 		self.speak_dialog("start.game")
 		# get lower bound
-		lowerBound = self.get_names("get.lower")
+		lowerBound = self.get_stops("get.lower")
 		# get myfriendname
-		upperBound = self.get_names("get.upper")
+		upperBound = self.get_stops("get.upper")
 	def  enter_source_destination(stops):
 		while True:
         		source = lowerBound
-#         		if source in stops:
-#             			while True:
-#                 			destination = upperBound
-#                 			if destination in stops:
-#                     				return source, destination
-#                 			else:
-#                     				self.speak('Could you please enter a valid destination stop')
-#                     				continue
-#        			 else:
-#             			self.speak('Could you please enter a valid boarding point')
-#            			continue
+        		if source in stops:
+            			while True:
+                			destination = upperBound
+                			if destination in stops:
+                    				return source, destination
+                			else:
+                    				self.speak('Could you please enter a valid destination stop')
+                    				continue
+       			 else:
+            			self.speak('Could you please enter a valid boarding point')
+           			continue
 
 # 	source, destination = enter_source_destination(stops)
 # 	self.speak('The sourceing point is '+ source+ 'and the destination is '+ destination)
