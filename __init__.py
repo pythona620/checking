6c1acc85fb39947a468e692248b94f6d83a16009
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-29 10:18:57 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,51 @@
 from adapt.intent import IntentBuilder
-from mycroft import intent_handler
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util.log import LOG, getLogger
 
-class PotatoSkill(MycroftSkill):
-	@intent_handler(IntentBuilder().require(PythonPerson).require(WhereFrom))
-		
-    	def handle_from(self, message):
-        # PythonPerson can be any of the Monty Python members
-        python = message.data.get('PythonPerson')
-        self.speak('He is from {}'.format(from_dict[python]))
-        self.set_context('PythonPerson', python)
-        self.set_context('Location', from_dict[python])
-	
-	
+from random import randint
+
+__author__ = 'pthona'
+LOGGER = getLogger(__name__)
+
+class NumberGuessSkill(MycroftSkill):
+# 	lowerBound = 0
+# 	upperBound = 100
+# 	answer = 0
+# 	userGuess = 0
+	def get_numerical_response(self, dialog):
+# 		while True:
+# 			val = self.get_response(dialog)
+# 			try:
+# 				val = int(val)
+# 				return val
+# 			except ValueError:
+# 				self.speak_dialog("invalid.input")
+# 			except:
+# 				self.speak_dialog("input.error")
+	@intent_handler(IntentBuilder("").require("NumberGuess").optionally("Play").optionally("Suggest"))
+	def handle_start_game_intent(self, message):
+		self.speak_dialog("start.game")
+
+		# get lower bound
+		lowerBound = self.get_numerical_response("get.lower")
+		# get upper bound
+		upperBound = self.get_numerical_response("get.upper")
+		answer = lowerBound + upperBound
+# 		answer = randint(lowerBound, upperBound)
+# 		userGuess = lowerBound - 1
+# 		while userGuess != answer:
+# 			userGuess = self.get_numerical_response("guess")
+# 			if userGuess < answer:
+# 				self.speak_dialog("too.low")
+# 			elif userGuess > answer:
+# 				self.speak_dialog("too.high")
+# 		self.speak_dialog("correct")
+
+	def stop(self):
+# 		lowerBound, upperBound = 0, 100
+# 		answer = 0
+# 		userGuess = answer
+# 		return True
+		pass
 def create_skill():
-    return PotatoSkill()
+	return NumberGuessSkill()
