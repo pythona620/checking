a5bbdfb22bf3403c1ffe9dfda3bb5cff49005cef
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-29 12:42:44 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,17 @@
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG, getLogger
-
 from random import randint
-
 __author__ = 'python'
 LOGGER = getLogger(__name__)
-
 class NumberGuessSkill(MycroftSkill):
+	stops = ["vizag","hyd","sec"]
 	def get_numerical_response(self, dialog):
-# 		while True:
+		while True:
 			val = self.get_response(dialog)
+			if lower in stops:
+				self.speak_dialog("invalid.input")
+				
 # 			try:
 # 				val = int(val)
 # 				return val
@@ -18,18 +19,16 @@ class NumberGuessSkill(MycroftSkill):
 # 				self.speak_dialog("invalid.input")
 # 			except:
 # 				self.speak_dialog("input.error")
-
 	@intent_handler(IntentBuilder("").require("NumberGuess").optionally("Play").optionally("Suggest"))
 	def handle_start_game_intent(self, message):
 		self.speak_dialog("start.game")
-
 		# get lower bound
 		lowerBound = self.get_numerical_response("get.lower")
 		# get upper bound
 		upperBound = self.get_numerical_response("get.upper")
+		self.speak(lowerBound)
 # 		if lowerBound is not None:
 # 		answer = (lowerBound + upperBound)
-		self.speak(lowerBound)
 # 		userGuess = lowerBound - 1
 # 		while userGuess != answer:
 # 			userGuess = self.get_numerical_response("guess")
