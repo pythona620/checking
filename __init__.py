78eebeeffa2428cb0a6496b083be882c698c7ae8
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-29 18:19:55 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,49 +5,39 @@ from random import randint
 __author__ = 'python'
 LOGGER = getLogger(__name__)
 class checkingSkill(MycroftSkill):
-	stops = {"vizag","sec","hyd","abc"}
-# 	upperBound = {"vizag","sec","hyd","abc"}
-# 	answer = 0
-# 	userGuess = 0
+# 	stops = {"vizag","sec","hyd","abc"}
 	def get_numerical_response(self, dialog):
 		while True:
 			val = self.get_response(dialog)
-# 			try:
-# 				val = int(val)
 				return val
-# 			except ValueError:
-# 				self.speak_dialog("invalid.input")
-# 			except:
-# 				self.speak_dialog("input.error")
 	@intent_handler(IntentBuilder("").require("NumberGuess").optionally("Play").optionally("Suggest"))
 	def handle_start_game_intent(self, message):
 		self.speak_dialog("start.game")
 		# get lower bound
-		lowerBound = self.get_numerical_response("get.lower")
-		self.speak(lowerBound)
-		# get upper bound
-		upperBound = self.get_numerical_response("get.upper")
-		self.speak(upperBound)
-# 		answer = randint(lowerBound, upperBound)
-# 		userGuess = lowerBound - 1
+# 		source = self.get_numerical_response("get.lower")
+# 		self.speak(lowerBound)
+# 		# get upper bound
+# 		destination = self.get_numerical_response("get.upper")
+	def  enter_source_destination(stops):
 		while True:
-        		source = lowerBound
+        		source = self.get_numerical_response("get.lower")
         		if source in stops:
             			while True:
-                			destination = upperBound
+                			destination = self.get_numerical_response('get.upper')
                 			if destination in stops:
-                    				return lowerBound, upperBound
+                    				return source, destination
                 			else:
-                    				self.speak("Could you please enter a valid destination stop")
+                    				print('Could you please enter a valid destination stop')
                     				continue
-        		else:
-            			self.speak("Could you please enter a valid boarding point")
-            		continue
+       			 	else:
+            				print('Could you please enter a valid boarding point')
+           			continue
+
+	stops = ['vizag', 'hyderabad', 'vijayawada']
+	source, destination = enter_source_destination(stops)
+	self.speak('The sourceing point is '+ source+ 'and the destination is '+ destination)
+
 	def stop(self):
-		print("The sourceing point is "+ source+"and the destination is"+ destination)
-# 		lowerBound, upperBound = 0, 100
-# 		answer = 0
-# 		userGuess = answer
-		return True
+		pass
 def create_skill():
 	return checkingSkill()
