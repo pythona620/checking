cf6fc0363aebe8c24273e92474193c8b90e67d2a
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-29 17:35:00 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,12 +7,18 @@ from random import randint
 __author__ = 'python'
 LOGGER = getLogger(__name__)
 
+
+stops = {"vizag","hyd","sec","vijayawada"}
 class NumberGuessSkill(MycroftSkill):
 
 	def get_numerical_response(self, dialog):
 		while True:
 			val = self.get_response(dialog)
-			
+	def get_stops(self, stops):
+		stoplist = []
+		if stops !=lower:
+			self.speak(tested ok)
+		
 	@intent_handler(IntentBuilder("").require("NumberGuess").optionally("Play").optionally("Suggest"))
 	def handle_start_game_intent(self, message):
 		self.speak_dialog("start.game")
