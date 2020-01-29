c45a959531a3700ae87622dfe4fed25551942eb1
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-29 17:44:03 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,21 +11,21 @@ LOGGER = getLogger(__name__)
 stops = {"vizag","hyd","sec","vijayawada"}
 class NumberGuessSkill(MycroftSkill):
 
-	def get_numerical_response(self, dialog):
-		while True:
-			val = self.get_response(dialog)
+# 	def get_numerical_response(self, dialog):
+# 		while True:
+# 			val = self.get_response(dialog)
 # 	def get_stops(self, stops):
 # 		stoplist = []
 # 		if stops !=lower:
 # 			self.speak(tested ok)
 		
 	@intent_handler(IntentBuilder("").require("NumberGuess").optionally("Play").optionally("Suggest"))
-	def handle_start_game_intent(self, message):
-		self.speak_dialog("start.game")
-
-		# get lower bound
+# 	def handle_start_game_intent(self, message):
+# 		self.speak_dialog("start.game")
+	def get_numerical_response(self, dialog):
+# 		# get lower bound
 		lowerBound = self.get_numerical_response("get.lower")
-		# get upper bound
+# 		# get upper bound
 		upperBound = self.get_numerical_response("get.upper")
 		if lowerBound is not None:
 			self.speak(lowerBound + " "+upperBound)
