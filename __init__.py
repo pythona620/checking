2b9f7a86234993b92198be87bfcdde5b51ec86ff
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-30 10:32:21 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from random import randint
 __author__ = 'python'
 LOGGER = getLogger(__name__)
 class checkingSkill(MycroftSkill):
-	stops = {"vizag","sec","hyd","abc"}
+	
 	def get_stops(self, dialog):  #get input from mic
 		yip = self.get_response(dialog) 
 		return yip
@@ -16,26 +16,26 @@ class checkingSkill(MycroftSkill):
 		lowerBound = self.get_stops("get.lower")
 		# get myfriendname
 		upperBound = self.get_stops("get.upper")
-# 	def  enter_source_destination(self,stops):
-# 		while True:
-#         		source = lowerBound
-# 			self.speak('Could you please enter a valid destination stop')
-# 			self.speak(source)
-#         		if source in stops:
-# 				while True:
-# 					destination = upperBound
-#                 			if destination in stops:
-#                     				return source, destination
-#                 			else:
-#                     				self.speak('Could you please enter a valid destination stop')
-#                     				continue
-#        		else:
-#             			self.speak('Could you please enter a valid boarding point')
-#            			continue
-
-# 	source, destination = enter_source_destination(stops)
-# 	self.speak('The sourceing point is '+ source+ 'and the destination is '+ destination)
-
+	def  enter_source_destination(self,stops):
+		while True:
+        		source = lowerBound
+			self.speak('Could you please enter a valid destination stop')
+			self.speak(source)
+        		if source in stops:
+				while True:
+					destination = upperBound
+                			if destination in stops:
+                    				return source, destination
+                			else:
+                    				self.speak('Could you please enter a valid destination stop')
+                    				continue
+       		else:
+            			self.speak('Could you please enter a valid boarding point')
+           			continue
+	stops = {"vizag","sec","hyd","abc"}
+	source, destination = enter_source_destination(stops)
+	self.speak('The sourceing point is '+ source+ 'and the destination is '+ destination)
+	
 	
 def create_skill():
 	return checkingSkill()
